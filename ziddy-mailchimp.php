b4fb929c9abbf105faf02387b498fc8de6fba89c
Zachary Townsend <zach@verbbrands.com>
2018-06-18 15:13:51 +0200

    First commit

---
+++ ziddy-mailchimp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Ziddy Mailchimp
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     A Wordpress plugin for managing Mailchimp subscriptions
+ * Author:          Zach Townsend
+ * Author URI:      zachtownsend.net
+ * Text Domain:     ziddy-mailchimp
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ziddy_Mailchimp
+ */
+
+// Your code starts here.
